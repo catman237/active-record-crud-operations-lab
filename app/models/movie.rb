43871bab5aa9b0baad25e65c8fc1c871978f9077
Greg <Ghughes610@gmail.com>
2021-06-14 20:25:08 -0600

    commit tree 71309bd1c77feb2399a565a0859aab84e958bd84

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ActiveRecord::Base
+    blade_runner = Movie.create(
+     title: "blade runner",
+     release_date: 01/21/22, 
+     director: "Martin Johnson",
+     lead: "Brad Pitt",
+     in_theaters: false
+    )
 end
